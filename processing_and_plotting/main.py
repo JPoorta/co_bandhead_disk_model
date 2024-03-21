830f657c191625ba95f54e184d4b1bb23e150dc6
Hanneke Poorta <hpoorta@gmail.com>
2024-03-21 18:18:34 +0100

    Version to run and save all the models for the P4 plot.

--- processing_and_plotting/main.py
+++ processing_and_plotting/main.py
@@ -5,8 +5,10 @@ import plotting_routines as pltr
 
 def run():
 
-    # run_gr.run_quick_test_grid(gr_list.common_test_grid(), **{"quick_plots": True})
-    pltr.plot_cum_flux_grid(gr_list.common_test_grid())
+    run_gr.save_run_variation_around_one_model(grid=gr_list.grid_for_main_figure_p4())
+    run_gr.save_grid_including_13co(gr_list.grid_for_main_figure_p4())
+    run_gr.add_saved_isotope_grid(gr_list.grid_for_main_figure_p4())
+    # pltr.plot_cum_flux_grid(gr_list.common_test_grid())
 
     return
 
