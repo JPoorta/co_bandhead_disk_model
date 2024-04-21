b67aab0adcea052855687fdd6cf7c7c0556407ad
Hanneke Poorta <hpoorta@gmail.com>
2024-04-21 21:12:59 +0200

    Added four extra grids to make fundamental predictions for B243 and B268.

--- grids/list_of_grids.py
+++ grids/list_of_grids.py
@@ -30,7 +30,7 @@ def grid_including_13co(species, iso_ratio, grid=None):
     For a given grid, adjust the saving folder, the isotopologue, and the iso_ratio parameters. Return the updated grid.
 
     :param species: (str) name of isotopologue
-    :param iso_ratio: (int) N12CO/Nisotopologue (only 13CO so far)
+    :param iso_ratio: (int) N12CO/N(isotopologue) (only 13CO so far)
     :param grid: (tuple) the three dictionaries containing all that specifies a grid. Default is the one for the main
     figure paper 4.
     :return:
@@ -62,8 +62,8 @@ def grid_for_main_figure_p4(star=None):
     grid_params, all_params = cfg.get_default_params(star)
 
     # Set the parameters to be tested.
-    test_param_dict = {"t1": [600, 700, 800, 900],
-                       "ti": [2000, 3000, 4000, 5000],
+    test_param_dict = {"ti": [2000, 3000, 4000, 5000],
+                       "t1": [600, 700, 800, 900],
                        "ni": [5e23, 3.9e24, 3e25, 6.5e26, 5e27],
                        "q": [1, -0.5, -1, -1.5],
                        "ri": [0.261, 0.5, 1],
@@ -162,8 +162,8 @@ def plots_for_intro_grid():
 
     test_param_dict = {"Rmin_in": [0.261]}  # , 0.446, 0.667
 
-    all_params["vupper"] = [1,2,3]  # [2] for stages in bh plot
-    all_params["vlower"] = [0,0,0]  # [0] for stages in bh plot
+    all_params["vupper"] = [1, 2, 3]  # [2] for stages in bh plot
+    all_params["vlower"] = [0, 0, 0]  # [0] for stages in bh plot
     all_params["dust"] = True
     grid_params["p"] = -2
     grid_params["t1"] = None  # 800  #
@@ -173,5 +173,84 @@ def plots_for_intro_grid():
     all_params["dF"] = None  # ""
     all_params["num_CO"] = 5  # for the tau for different temperatures;
     # all_params["num_CO"] = 2 for ring in stages of bh plot
+
+    return grid_params, all_params, test_param_dict
+
+
+def fid_model_B243(test_param_dict=None):
+    grid_params, all_params = cfg.get_default_params("B243")
+    if test_param_dict is None:
+        test_param_dict = {"t1": [800]}
+    vupper, vlower = vibrational_levels_for_3_ro_vib_series()
+    grid_params["t1"] = 800
+    grid_params["a"] = -11
+    all_params["vupper"] = vupper
+    all_params["vlower"] = vlower
+    all_params["Rmax_in"] = 100
+    all_params["dF"] = ""
+    all_params["dust"] = True
+    all_params["convolve"] = False
+    all_params["maxmin"] = [100, -100]
+    all_params["print_Rs"] = False
+    all_params["save"] = "prediction_p4"
+    all_params["save_reduced_flux"] = False
+
+    return grid_params, all_params, test_param_dict
+
+
+def fid_model_B268(test_param_dict=None):
+    grid_params, all_params = cfg.get_default_params("B268")
+    if test_param_dict is None:
+        test_param_dict = {"t1": [1000], }
+    vupper, vlower = vibrational_levels_for_3_ro_vib_series()
+    grid_params["t1"] = 1000
+    grid_params["a"] = -8
+    all_params["vupper"] = vupper
+    all_params["vlower"] = vlower
+    all_params["Rmax_in"] = 100
+    all_params["dF"] = ""
+    all_params["dust"] = True
+    all_params["convolve"] = False
+    all_params["maxmin"] = [100, -100]
+    all_params["print_Rs"] = False
+    all_params["save"] = "prediction_p4"
+    all_params["save_reduced_flux"] = False
+
+    return grid_params, all_params, test_param_dict
+
+
+def fid_model_B268_no_dust(test_param_dict=None):
+    grid_params, all_params = cfg.get_default_params("B268")
+    if test_param_dict is None:
+        test_param_dict = {"dust": [False, ], }
+    vupper, vlower = vibrational_levels_for_3_ro_vib_series()
+    grid_params["t1"] = 1000
+    grid_params["a"] = -8
+    all_params["vupper"] = vupper
+    all_params["vlower"] = vlower
+    all_params["Rmax_in"] = 100
+    all_params["dF"] = "no_dust"
+    all_params["dust"] = False
+
+    return grid_params, all_params, test_param_dict
+
+
+def fid_model_B275_no_dust(test_param_dict=None):
+    """
+    Grid most commonly used in preparation for P4 to test stuff with adaptable test parameter grid.
+    Note that the arrays for cumulative flux calculation are being saved for this grid, but not the final models.
+
+    :return:
+    """
+
+    grid_params, all_params = cfg.get_default_params("B275")
+    if test_param_dict is None:
+        test_param_dict = {"dust": [False]}
+    vupper, vlower = vibrational_levels_for_3_ro_vib_series()
+    all_params["vupper"] = vupper
+    all_params["vlower"] = vlower
+    all_params["Rmax_in"] = 100
+    all_params["dF"] = "_no_dust"
+    all_params["dust"] = False
 
     return grid_params, all_params, test_param_dict
